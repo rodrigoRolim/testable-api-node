1f94d536ec8a3e844e08e9d8508abb390c9bb561
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-09 15:16:16 -0200

    added useNewUrlParse to mongoClient

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
 mongoose.Promise = Promise
-const mongodUrl = process.env.MONGODB_URL || 'mongod://localhost/test'
-const connect = () => mongoose.connect(mongodUrl)
+const mongodUrl = process.env.MONGODB_URL || 'mongodb://localhost/test'
+const connect = () => mongoose.connect(mongodUrl, { useNewUrlParser: true })
 
 export default {
   connect
