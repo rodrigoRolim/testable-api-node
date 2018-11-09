3c2b6d22def94ae44ffb603df59ba1ee981fa25f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-09 18:52:36 -0200

    refactor test to implement real business logic

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,17 @@
 class ProductsController {
   
+  constructor(Product) {
+    this.Product = Product
+  }
   get(req, res) {
-    return res.send([{
-      name: 'Default product',
-      description: 'product description',
-      price: 100
-    }])
+    // return res.send([{
+    //   name: 'Default product',
+    //   description: 'product description',
+    //   price: 100
+    // }])
+    return this.Product.find({})
+        .then(products => res.send(products))
+        .catch(err => res.status(400).send(err.message))
   }
 }
 
--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,6 +1,6 @@
 import ProductsController from '../../../src/controllers/products'
 import sinon from 'sinon'
-
+import Product from '../../../src/models/products'
 describe('Controllers: Products', () => {
   const defaultProduct = [{
     name: 'Default product',
@@ -8,16 +8,39 @@ describe('Controllers: Products', () => {
     price: 100
   }]
   describe('get() products', () => {
-    it('should return a list of products', () => {
+    it('should call send with a list of products', () => {
       const request = {}
       const response = {
         send: sinon.spy()
-      } 
-      const productsControllers = new ProductsController()
-      productsControllers.get(request, response)
+      }
+      Product.find = sinon.stub() 
+      //const productsControllers = new ProductsController()
+      //productsControllers.get(request, response)
+      Product.find.withArgs({}).resolves(defaultProduct)
+      const productsControllers = new ProductsController(Product)
+      return productsControllers.get(request, response)
+          .then(() => {
+            sinon.assert.calledWith(response.send, defaultProduct)
+          })
+      //expect(response.send.called).to.be.true
+      //expect(response.send.calledWith(defaultProduct)).to.be.true
+    })
+    it('should return 400 when an error occurs', () => {
+      const request = {}
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      }
 
-      expect(response.send.called).to.be.true
-      expect(response.send.calledWith(defaultProduct)).to.be.true
+      response.status.withArgs(400).returns(response)
+      Product.find = sinon.stub()
+      Product.find.withArgs({}).rejects({ message: 'Error' })
+
+      const productsController = new ProductsController(Product)
+      return productsController.get(request, response)
+        .then(() => {
+          sinon.assert.calledWith(response.send, 'Error')
+        })
     })
   })
 })
