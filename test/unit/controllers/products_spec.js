db7ebd41f15bd160c85f96174dc9cf23b18dd5cc
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-09 14:41:08 -0200

    added produts controller unit test

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,7 +1,23 @@
+import ProductsController from '../../../src/controllers/products'
+import sinon from 'sinon'
+
 describe('Controllers: Products', () => {
+  const defaultProduct = [{
+    name: 'Default product',
+    description: 'product description',
+    price: 100
+  }]
   describe('get() products', () => {
     it('should return a list of products', () => {
-        
+      const request = {}
+      const response = {
+        send: sinon.spy()
+      } 
+      const productsControllers = new ProductsController()
+      productsControllers.get(request, response)
+
+      expect(response.send.called).to.be.true
+      expect(response.send.calledWith(defaultProduct)).to.be.true
     })
   })
 })
