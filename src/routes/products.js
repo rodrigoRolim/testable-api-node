abad0fd09bda3b4c804e883e3fe40fc636635165
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-11 13:32:21 -0200

    create getById and create routes

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,5 +6,6 @@ const router = express.Router()
 const productsController = new ProductsController(Product)
 
 router.get('/', (req, res) => productsController.get(req, res))
-
+router.get('/:id', (req, res) => productsController.getById(req, res))
+router.post('/', (req, res) => productsController.create(req, res))
 export default router
