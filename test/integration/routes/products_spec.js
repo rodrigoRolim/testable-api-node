c4e350da44ac72afda9ba51c6ffd4c43d1357c95
rodrigoRolim <rodrigorolimveras92@gmail.com>
2018-11-11 13:32:39 -0200

    alterd defaultProduct

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,24 +1,77 @@
+import Product from "../../../src/models/products";
+
 describe('Routes: Products', () => {
+  let request
+  const defaultId = '56cb91bdc3464f14678934ca';
+  before(() => {
+    return setupApp()
+             .then(app => {
+               request = supertest(app)
+             })
+  })
   const defaultProduct = {
     name: 'Default product',
     description: 'product description',
     price: 100
+  };
+  const expectedProduct = {
+    __v: 0,
+    _id: defaultId,
+    name: 'Default product',
+    description: 'product description',
+    price: 100
   }
+  beforeEach(() => {
+    const product = new Product(defaultProduct)
+    product._id = '56cb91bdc3464f14678934ca'
+    return Product.remove({})
+      .then(() => product.save())
+  })
+  afterEach(() => Product.remove({})) 
   describe('GET /products', () => {
-    let request
-    before(() => {
-      return setupApp()
-               .then(app => {
-                 request = supertest(app)
-               })
-    })
+  
     it('should return list of products', done => {
       request
         .get('/products')
         .end((err, res) => {
-          expect(res.body[0]).to.eql(defaultProduct)
+          //expect(res.body[0]).to.eql(defaultProduct)
+          expect(res.body).to.eql([expectedProduct])
           done(err);
         })
+    })
+    context('when an id is especified', done => {
+      it('should return 200 with one product', done => {
+        request
+          .get(`/products/${defaultId}`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(200)
+            expect(res.body).to.eql([expectedProduct])
+            done(err)
+          })
+      })
+    })
+  })
+  describe('POST /products', () => {
+    context('when posting a product', () => {
+      it('should return a new product with status code 201', done => {
+        const customId = '56cb91bdc3464f14678934ba'
+        const newProduct = Object.assign({}, { _id: customId, __v: 0}, defaultProduct)
+        const expectedSavedProduct = {
+          __v: 0,
+          _id: customId,
+          name: 'Default product',
+          description: 'product description',
+          price: 100
+        }
+        request
+          .post('/products')
+          .send(newProduct)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(201)
+            expect(res.body).to.eql(expectedSavedProduct)
+            done(err)
+          })
+      })
     })
   })
 })
